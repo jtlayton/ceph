84b8a8baf1079daa120dbce266ce457ca6eec1c0
Greg Farnum <gfarnum@redhat.com>
2016-07-27 15:34:43 -0700

    client: use get_gid()/get_uid() instead of pick_my_perms() directly
    By the time we're done we don't want to be setting any permissions in the
    Client code directly, and this open-coding will make it more obvious if
    we miss something (when removing the get_uid() and get_gid() functions).
    
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -6273,7 +6273,7 @@ int Client::mkdirs(const char *relpath, mode_t mode)
   tout(cct) << relpath << std::endl;
   tout(cct) << mode << std::endl;
 
-  UserPerm perms = pick_my_perms();
+  UserPerm perms(get_uid(), get_gid());
 
   //get through existing parts of path
   filepath path(relpath);
@@ -6325,7 +6325,7 @@ int Client::rmdir(const char *relpath)
   string name = path.last_dentry();
   path.pop_dentry();
   InodeRef dir;
-  UserPerm perms = pick_my_perms();
+  UserPerm perms(get_uid(), get_gid());
   int r = path_walk(path, &dir, perms);
   if (r < 0)
     return r;
@@ -6348,7 +6348,7 @@ int Client::mknod(const char *relpath, mode_t mode, dev_t rdev)
   string name = path.last_dentry();
   path.pop_dentry();
   InodeRef dir;
-  UserPerm perms = pick_my_perms();
+  UserPerm perms(get_uid(), get_gid());
   int r = path_walk(path, &dir, perms);
   if (r < 0)
     return r;
@@ -6373,7 +6373,7 @@ int Client::symlink(const char *target, const char *relpath)
   string name = path.last_dentry();
   path.pop_dentry();
   InodeRef dir;
-  UserPerm perms = pick_my_perms();
+  UserPerm perms(get_uid(), get_gid());
   int r = path_walk(path, &dir, perms);
   if (r < 0)
     return r;
