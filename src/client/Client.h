55bf0cfb5c97301837d7dcb5e6fdf191925eb359
Yan, Zheng <zyan@redhat.com>
2018-03-15 11:31:01 +0800

    mds: allow client to specify its session timeout
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>

--- src/client/Client.h
+++ src/client/Client.h
@@ -601,6 +601,7 @@ protected:
 
  public:
   void set_uuid(const std::string& uuid);
+  void set_timeout(unsigned timeout);
   int start_reclaim(const std::string& uuid, unsigned flags);
   void finish_reclaim();
 
