926407dd5ff1e22af844d4fad7a40fb6adf62bc5
Yan, Zheng <zyan@redhat.com>
2018-05-04 11:11:35 +0800

    mds: allow client to specify its session timeout
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -13932,6 +13932,15 @@ void Client::set_uuid(const std::string& uuid)
   _close_sessions();
 }
 
+// called before mount. 0 means infinite
+void Client::set_timeout(unsigned timeout)
+{
+  Mutex::Locker l(client_lock);
+  assert(initialized);
+
+  metadata["timeout"] = stringify(timeout);
+}
+
 // called before mount
 int Client::start_reclaim(const std::string& uuid, unsigned flags)
 {
