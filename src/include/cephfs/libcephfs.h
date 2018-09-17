e24917a8a2b75449f3d715c43593c1ae749842b0
Jeff Layton <jlayton@redhat.com>
2018-09-17 14:22:19 -0400

    client: expose CEPH_SETATTR_MTIME_NOW/CEPH_SETATTR_ATIME_NOW
    Currently, userland apps can't set CEPH_SETATTR_MTIME_NOW and
    CEPH_SETATTR_ATIME_NOW as they aren't defined in the header file. Add
    them so that userland applications can use them appropriately.
    
    While we're in there, redefine all of the values in terms of bitshifts,
    for better consistency with the internal header file.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>

--- src/include/cephfs/libcephfs.h
+++ src/include/cephfs/libcephfs.h
@@ -109,14 +109,16 @@ struct CephContext;
 
 /* setattr mask bits */
 #ifndef CEPH_SETATTR_MODE
-# define CEPH_SETATTR_MODE	1
-# define CEPH_SETATTR_UID	2
-# define CEPH_SETATTR_GID	4
-# define CEPH_SETATTR_MTIME	8
-# define CEPH_SETATTR_ATIME	16
-# define CEPH_SETATTR_SIZE	32
-# define CEPH_SETATTR_CTIME	64
-# define CEPH_SETATTR_BTIME	512
+# define CEPH_SETATTR_MODE	(1 << 0)
+# define CEPH_SETATTR_UID	(1 << 1)
+# define CEPH_SETATTR_GID	(1 << 2)
+# define CEPH_SETATTR_MTIME	(1 << 3)
+# define CEPH_SETATTR_ATIME	(1 << 4)
+# define CEPH_SETATTR_SIZE	(1 << 5)
+# define CEPH_SETATTR_CTIME	(1 << 6)
+# define CEPH_SETATTR_MTIME_NOW	(1 << 7)
+# define CEPH_SETATTR_ATIME_NOW	(1 << 8)
+# define CEPH_SETATTR_BTIME	(1 << 9)
 #endif
 
 /* define error codes for the mount function*/
