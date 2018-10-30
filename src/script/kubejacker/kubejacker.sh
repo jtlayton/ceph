98480a93904da8d67199778b77b36a141f8e6d4d
Jeff Layton <jlayton@redhat.com>
2018-10-30 08:28:43 -0400

    kubejacker: strip it down to just update python
    Signed-off-by: Jeff Layton <jlayton@redhat.com>

--- src/script/kubejacker/kubejacker.sh
+++ src/script/kubejacker/kubejacker.sh
@@ -16,12 +16,12 @@ BASEIMAGE="${BASEIMAGE:-$DEFAULT_BASEIMAGE}"
 
 # PREREQUISITE: a repo that you can push to.  You are probably running
 # a local docker registry that your kubelet nodes also have access to.
-if [ -z "$REPO" ]
-then
-    echo "ERROR: no \$REPO set!"
-    echo "Run a docker repository and set REPO to <hostname>:<port>"
-    exit -1
-fi
+# if [ -z "$REPO" ]
+# then
+#     echo "ERROR: no \$REPO set!"
+#     echo "Run a docker repository and set REPO to <hostname>:<port>"
+#     exit -1
+# fi
 
 # The output image name: this should match whatever is configured as
 # the image name in your Rook cluster CRD object.
@@ -38,20 +38,20 @@ sed -i s@BASEIMAGE@$BASEIMAGE@ kubejacker/Dockerfile
 
 # TODO: let user specify which daemon they're interested
 # in -- doing all bins all the time is too slow and bloaty
-BINS="ceph-mgr ceph-mon ceph-mds ceph-osd rados radosgw-admin radosgw"
-pushd bin
-strip $BINS  #TODO: make stripping optional
-tar czf $BUILDPATH/kubejacker/bin.tar.gz $BINS
-popd
+# BINS="ceph-mgr ceph-mon ceph-mds ceph-osd rados radosgw-admin radosgw"
+# pushd bin
+# strip $BINS  #TODO: make stripping optional
+# tar czf $BUILDPATH/kubejacker/bin.tar.gz $BINS
+# popd
 
 # We need ceph-common to support the binaries
 # We need librados/rbd to support mgr modules
 # that import the python bindings
-LIBS="libceph-common.so.0 libceph-common.so librados.so.2 librados.so librados.so.2.0.0 librbd.so librbd.so.1 librbd.so.1.12.0"
-pushd lib
-strip $LIBS  #TODO: make stripping optional
-tar czf $BUILDPATH/kubejacker/lib.tar.gz $LIBS
-popd
+# LIBS="libceph-common.so.0 libceph-common.so librados.so.2 librados.so librados.so.2.0.0 librbd.so librbd.so.1 librbd.so.1.12.0"
+# pushd lib
+# strip $LIBS  #TODO: make stripping optional
+# tar czf $BUILDPATH/kubejacker/lib.tar.gz $LIBS
+# popd
 
 pushd ../src/pybind/mgr
 find ./ -name "*.pyc" -exec rm -f {} \;
@@ -59,28 +59,26 @@ find ./ -name "*.pyc" -exec rm -f {} \;
 tar --exclude=node_modules --exclude=tests --exclude-backups -czf $BUILDPATH/kubejacker/mgr_plugins.tar.gz *
 popd
 
-ECLIBS="libec_*.so*"
-pushd lib
-strip $ECLIBS  #TODO: make stripping optional
-tar czf $BUILDPATH/kubejacker/eclib.tar.gz $ECLIBS
-popd
+# ECLIBS="libec_*.so*"
+# pushd lib
+# strip $ECLIBS  #TODO: make stripping optional
+# tar czf $BUILDPATH/kubejacker/eclib.tar.gz $ECLIBS
+# popd
 
-CLSLIBS="libcls_*.so*"
-pushd lib
-strip $CLSLIBS  #TODO: make stripping optional
-tar czf $BUILDPATH/kubejacker/clslib.tar.gz $CLSLIBS
-popd
+# CLSLIBS="libcls_*.so*"
+# pushd lib
+# strip $CLSLIBS  #TODO: make stripping optional
+# tar czf $BUILDPATH/kubejacker/clslib.tar.gz $CLSLIBS
+# popd
 
 pushd kubejacker
-docker build -t $REPO/$IMAGE:$TAG .
+docker build -t $IMAGE:$TAG .
 popd
 
 # Push the image to the repository
-docker tag $REPO/$IMAGE:$TAG $REPO/$IMAGE:latest
-docker push $REPO/$IMAGE:latest
-docker push $REPO/$IMAGE:$TAG 
+docker tag $IMAGE:$TAG $IMAGE:master
 
 # Finally, bounce the containers to pick up the new image
-kubectl -n $NAMESPACE delete pod -l app=rook-ceph-mds
+# kubectl -n $NAMESPACE delete pod -l app=rook-ceph-mds
 kubectl -n $NAMESPACE delete pod -l app=rook-ceph-mgr
-kubectl -n $NAMESPACE delete pod -l app=rook-ceph-mon
+# kubectl -n $NAMESPACE delete pod -l app=rook-ceph-mon
